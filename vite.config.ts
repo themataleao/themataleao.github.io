bbc4b7d05b4aaf442970255521244cfa00f0c631
willson <marc@manthano.ai>
2023-03-26 20:15:58 +0200

    fixing base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/themataleao.github.io/',
   plugins: [react()],
 })
